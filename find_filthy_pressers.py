b3618df41bc76e93995e3301d93100a8c45b85cd
Alexander Putilin <avp-13@yandex.ru>
2015-05-29 19:18:59 +0300

    Initial version

---
+++ find_filthy_pressers.py
@@ -0,0 +1,58 @@
+import logging
+import argparse
+import time
+import praw
+
+
+# Downloads all the self posts from given subreddit
+def find_filthy_pressers(ts_interval, largest_timestamp):
+    r = praw.Reddit(user_agent='filthypresser project 0.1 by /u/godlikesme')
+    if largest_timestamp is None:
+        largest_timestamp = int(time.time()) + 12*3600
+    cts2 = largest_timestamp
+    cts1 = largest_timestamp - ts_interval
+    current_ts_interval = ts_interval
+    while True:
+        try:
+            search_results = list(r.search('timestamp:{}..{}'.format(cts1, cts2), subreddit='thebutton', syntax='cloudsearch'))
+        except Exception as e:
+            logging.exception(e)
+            continue
+
+        logging.info("Got {} submissions in interval {}..{}".format(len(search_results), cts1, cts2))
+        if len(search_results) == 25:
+            current_ts_interval /= 2
+            cts1 = cts2 - current_ts_interval
+            logging.debug("Reducing ts interval to {}".format(current_ts_interval))
+            continue
+
+        for submission in search_results:
+            submission.replace_more_comments(limit=None)
+            for c in submission.comments:
+                # TODO: use regex here
+                if 'filthy presser' in c.body or "Filthy presser" in c.body:
+                    print c.id, (c.permalink if 'permalink' in dir(c) else None), c.body, c.author_flair_text
+
+        cts2 = cts1
+        cts1 = cts2 - current_ts_interval
+
+        if cts1 < 0:
+            break
+
+        if len(search_results) <= 7:
+            current_ts_interval *= 2
+            logging.debug("Increasing ts interval to {}".format(current_ts_interval))
+
+
+def main():
+    logging.getLogger().setLevel(logging.INFO)
+
+    parser = argparse.ArgumentParser(description='filthy presser app/bot')
+    parser.add_argument("--timestamp_interval", dest="timestamp_interval", type=int, required=True)
+    parser.add_argument("--largest_timestamp", dest="largest_timestamp", type=int, required=False, default=None)
+    args = parser.parse_args()
+
+    find_filthy_pressers(args.timestamp_interval, args.largest_timestamp)
+
+if __name__ == "__main__":
+    main()
