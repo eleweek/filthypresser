4b4784b9738dba15baec5b1c5c80cadea41af272
Alexander Putilin <avp-13@yandex.ru>
2015-05-30 04:44:52 +0300

    Add simple search functionality

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, g
 from flask_bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import or_, and_
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
+from wtforms import StringField, SubmitField
+from flask_wtf import Form
 import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -41,11 +44,21 @@ class Submission(db.Model):
     created_utc = db.Column(db.DateTime())
 
 
+class SearchForm(Form):
+    query = StringField("Regex")
+    search_button = SubmitField('Search!')
+
+
+@app.before_request
+def create_search_form():
+    g.search_form = SearchForm()
+
+
 @app.route("/all_submissions/<int:page>")
 @app.route("/all_submissions", defaults={'page': 1})
 def all_submissions(page):
     submissions = Submission.query.order_by(Submission.id).paginate(page=page, per_page=20)
-    return render_template("index.html", submissions_count=Submission.query.count(), submissions=submissions)
+    return render_template("submissions.html", submissions_count=Submission.query.count(), submissions=submissions)
 
 
 @app.route("/filthy_pressers/<int:page>")
@@ -60,14 +73,36 @@ def filthy_pressers(page):
                                   .order_by(Submission.id.desc())\
                                   .paginate(page=page, per_page=20)  # NOQA
 
-    return render_template("index.html", submissions_count=Submission.query.count(), submissions=submissions)
+    return render_template("submissions.html",
+                           submissions_count=Submission.query.count(),
+                           submissions=submissions,
+                           heading="Find false greys")
 
 
 @app.route("/non_pressers/<int:page>")
 @app.route("/non_pressers", defaults={'page': 1})
 def non_pressers(page):
     submissions = Submission.query.filter_by(author_flair_text='non presser').order_by(Submission.id.desc()).paginate(per_page=20, page=page)
-    return render_template("index.html", submissions_count=Submission.query.count(), submissions=submissions)
+    return render_template("submissions.html",
+                           submissions_count=Submission.query.count(),
+                           submissions=submissions,
+                           heading="Convert non-pressers")
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if g.search_form.validate_on_submit():
+        return redirect(url_for("search_results", query=g.search_form.query.data))
+    else:
+        return "Invalid request :("
+
+
+@app.route('/search_results/<query>/<int:page>')
+@app.route('/search_results/<query>', defaults={"page": 1})
+def search_results(query, page):
+    submissions = Submission.query.filter(Submission.selftext.op("~")(query)).order_by(Submission.id.desc()).paginate(per_page=20, page=page)
+
+    return render_template("submissions.html", submissions_count=Submission.query.count(), submissions=submissions, heading="Search results for query [{}]".format(query))
 
 
 @app.route("/")
